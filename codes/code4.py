9b4696c91e1cf3caa6e78f44ccb315e83fadfffd
JBA-12 <102513876+JBA-12@users.noreply.github.com>
2022-05-01 19:06:18 +0530

    Add files via upload

---
+++ codes/code4.py
@@ -0,0 +1,19 @@
+#Python code to verify whether obtained values are correct or not!
+#Let, P(X=0) = Pr
+#P(X=1) = Pb
+
+#No.of red balls = 5 and No.of blue balls = 10 i.e.,Total no.of balls = 15 implies,
+Pr = 5/15 
+Pb = 10/15 
+
+#Checks for the probability condition i.e., less than or equals to 1 and greater than or equals to 0.
+if Pr<=1 and Pr>=0:
+   "continue"
+if Pb<=1 and Pb>=0:
+   "continue"
+
+#Checks whether the sum of probabilities of all random bvariables is 1.   
+p = Pr + Pb
+
+if p == 1:
+  print("P(X=0) = 1/3 and P(X=1) = 2/3") 
